ae4b69b2498352db0acb0c2c92bf68e153df47f9
Lexaik <ka34enko@gmail.com>
2024-11-01 20:27:41 +0400

    добавил конструктор

--- Heroes.Lib/Models/BaseHero.cs
+++ Heroes.Lib/Models/BaseHero.cs
@@ -6,4 +6,12 @@ public abstract class BaseHero
     public int Health { get; set; }
     public int Damage { get; set; }
     public bool IsDead { get; set; }
+
+    public BaseHero(string name, int health=100, int damage=10, bool isDead=false)
+    {
+        Name = name;
+        Health = health;
+        Damage = damage;
+        IsDead = isDead;
+    }
 }
