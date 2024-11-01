64856aae6796bb277f0951268c67729a0543af1b
Starinin Andrey <starinin-andrey@ya.ru>
2024-11-01 20:50:15 +0300

    Создание тестов для HeroesConfig

---
+++ Heroes.Lib.Test/HeroesConfigTest.cs
@@ -0,0 +1,24 @@
+﻿using Heroes.Lib.Config;
+
+namespace Heroes.Lib.Test;
+
+public class HeroesConfigTest
+{
+    [Fact]
+    public void Load_Test()
+    {
+        const string path = "heroes_config_test.json";
+        var expected = new HeroesConfig
+        {
+            Warrior = new ConfigItem
+            {
+                Health = 100,
+                Damage = 10
+            }
+        };
+
+        var actual = HeroesConfig.Load(path);
+
+        Assert.Equal(expected, actual);
+    }
+}
