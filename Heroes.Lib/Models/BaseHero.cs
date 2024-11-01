0796510290d7a25bc3677a82ea940abcec40d399
Starinin Andrey <starinin-andrey@ya.ru>
2024-11-01 20:10:36 +0300

    Добавление проверок в BaseHero

--- Heroes.Lib/Models/BaseHero.cs
+++ Heroes.Lib/Models/BaseHero.cs
@@ -1,9 +1,41 @@
-﻿namespace Heroes.Lib.Models;
+﻿using System;
+
+namespace Heroes.Lib.Models;
 
 public abstract class BaseHero
 {
-    public required string Name { get; init; }
-    public int Health { get; set; }
-    public int Damage { get; set; }
-    public bool IsDead { get; set; }
+    private readonly string _name;
+    public required string Name
+    {
+        get => _name;
+        init
+        {
+            if(string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Имя не может быть пустым!");
+            }
+            _name = value;
+        }
+    }
+
+    private int _health;
+    public int Health
+    {
+        get => _health;
+        set => _health = value < 0 ? 0 : value;
+    }
+
+    private int _damage;
+    public int Damage
+    {
+        get => _damage;
+        set => _damage = value < 0 ? 0 : value;
+    }
+
+    public bool IsDead => Health == 0;
+
+    public virtual void Attack(BaseHero enemy)
+    {
+        enemy.Health -= Damage;
+    }
 }
