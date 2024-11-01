87d63aacb61ffc6cb0ee1e0af60197f3d8417801
Starinin Andrey <starinin-andrey@ya.ru>
2024-11-01 18:58:19 +0300

    Добавление BaseHero.cs

---
+++ Heroes.Lib/Models/BaseHero.cs
@@ -0,0 +1,9 @@
+﻿namespace Heroes.Lib.Models;
+
+public abstract class BaseHero
+{
+    public required string Name { get; init; }
+    public int Health { get; set; }
+    public int Damage { get; set; }
+    public bool IsDead { get; set; }
+}
