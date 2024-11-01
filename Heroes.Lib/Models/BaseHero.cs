734f9ee9e88d106a92da0dfdaaed78009f41b4e4
Udovenko Mila <udovenko1201@gmail.com>
2024-11-01 19:20:26 +0300

    Добавление конструктора.

--- Heroes.Lib/Models/BaseHero.cs
+++ Heroes.Lib/Models/BaseHero.cs
@@ -1,4 +1,6 @@
-﻿namespace Heroes.Lib.Models;
+﻿using System;
+
+namespace Heroes.Lib.Models;
 
 public abstract class BaseHero
 {
@@ -6,4 +8,17 @@ public abstract class BaseHero
     public int Health { get; set; }
     public int Damage { get; set; }
     public bool IsDead { get; set; }
+
+    public BaseHero(string name, int health = 100, int damage = 10, bool isDead = false)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Name cannot be null or empty.");
+        }
+        this.Name = name;
+        this.Health = health;
+        this.Damage = damage;
+        this.IsDead = isDead;
+    }
 }
+
